bab56ef4247f0a32bacdc11edfca64370d1f8c99
Ruslan Zapolianenko <rusartrade@gmail.com>
2024-05-01 20:53:03 +0300

    commit3

--- src/main/java/com/example/zapolianenko/userapp/UserUpdateRequest.java
+++ src/main/java/com/example/zapolianenko/userapp/UserUpdateRequest.java
@@ -1,7 +1,6 @@
 package com.example.zapolianenko.userapp;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
---
+++ src/test/java/com/example/zapolianenko/userapp/UserUpdateRequestTest.java
@@ -0,0 +1,32 @@
+package com.example.zapolianenko.userapp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserUpdateRequestTest {
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getLastname() {
+    }
+
+    @Test
+    void getBirthDate() {
+    }
+
+    @Test
+    void getAddress() {
+    }
+
+    @Test
+    void getPhone() {
+    }
+}
